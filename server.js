129289e28ae86099eaaeaf64a2a1ccdec1808802
anandtiwari <anand.tiwari@gdn-commerce.com>
2018-04-06 14:51:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    app = express(),
+    port = process.argv[2] || 3002;
+
+app.use(express.static(__dirname + '/'));
+
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
